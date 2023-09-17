056b08d3a590f09f52f59e508748018f868fb908
gabriel-adel <gabriel_adelar@hotmail.com>
2023-09-17 20:06:32 -0300

    add screen

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from timer import Timer
-import time
+from kivy.clock import Clock
 from kivy.app import App
 from kivy.uix.label import Label
 
 class MyApp(App):
+    def __init__(self):
+        super(MyApp, self).__init__()
+        self.timer = Timer()
     def build(self):
-        return Label(text='ola mundo')
-timer = Timer()
-aa=3599
-sleep = 1
-start = True
-timer.timerWork()
-MyApp().run()
-while start:
-    #  logica vai ficara qui
-    res = timer.format_date(aa)
-    print(res)
-    aa+=1
-    time.sleep(sleep)
+        self.label = Label(text=f"Tempo: {self.timer.get_date()}")
+        Clock.schedule_interval(self.update_label,1)
+        return self.label
+    def update_label(self, dt):
+        self.label.text = f"Tempo: {self.get_current_time()}"
+        self.timer.add_timer()
+    def get_current_time(self):
+        return self.timer.format_date()
+         
+        
+# timer = Timer()
+# sleep = 1
+# start = True
+# timer = Timer()
+if __name__ == '__main__':
+    MyApp().run()
+# while start:
+#     #  logica vai ficara qui
+#     res = timer.format_date()
+#     timeRet = timer.add_timer()
+#     time.sleep(sleep)
+#     print(res)
     
-    if aa >= 25200:
-        start = False
+#     if timeRet >= 25200:
+#         start = False
